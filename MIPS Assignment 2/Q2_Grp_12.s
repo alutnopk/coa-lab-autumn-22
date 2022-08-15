35e573ca46d34a8f8d38ce5e8ed6813f5e0bdfab
Kartik Pontula <75580066+alutnopk@users.noreply.github.com>
2022-08-16 00:47:01 +0530

    update

--- MIPS Assignment 2/Q2_Grp_12.s
+++ MIPS Assignment 2/Q2_Grp_12.s
@@ -5,3 +5,16 @@
 # Group: 12
 # Members: Ashish Rekhani (20CS10010), Kartik Pontula (20CS10031)
 #####
+
+# Data Segment
+.data
+prompt:
+    .asciiz "Enter the value of k: "
+
+array:
+    .space 32
+
+# Text Segment
+.text
+.globl main
+main:
--- MIPS Assignment 2/Q3_Grp_12.s
+++ MIPS Assignment 2/Q3_Grp_12.s
@@ -5,3 +5,11 @@
 # Group: 12
 # Members: Ashish Rekhani (20CS10010), Kartik Pontula (20CS10031)
 #####
+
+# Data Segment
+.data
+
+# Text Segment
+.text
+.globl main
+main:
