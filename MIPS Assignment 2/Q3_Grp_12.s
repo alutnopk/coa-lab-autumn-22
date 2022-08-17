8b77df92ebb246e3071aaccb2eb189a06ec7132b
Kartik Pontula <75580066+alutnopk@users.noreply.github.com>
2022-08-17 14:58:46 +0530

    Updated

--- MIPS Assignment 2/Q3_Grp_12.s
+++ MIPS Assignment 2/Q3_Grp_12.s
@@ -9,11 +9,14 @@
 # Data Segment
 .data
 prompt_init: .asciiz "Enter four positive integers m, n, a and r: "
-
+blank_space: .asciiz " "
+newline: .asciiz "\n"
 # Text Segment
 .text
 .globl main
 main:
+    jal initStack
+
     li $v0, 4
     la $a0, prompt_init
     syscall # prompts user for input
@@ -33,15 +36,43 @@ main:
 
 
 initStack:
+    addi $sp, $sp, -4
+    sw $fp, ($sp)
+    move $fp, $sp
     jr $ra
 
 pushToStack:
+    addi $sp, $sp, -4
+    sw $a0, ($sp)
     jr $ra
 
 mallocInStack:
+    mul $t0, $a0, 4
+    sub $sp, $sp, $t0
+    move $v0, $sp
     jr $ra
 
 printMatrix:
+    move $t0, $a0
+    move $t1, $a1
+    move $t2, $a2 # current pointer is t2
+    move $t3, $zero # counter i is t3
+    print_outer_loop:
+        bge $t3, $a0, print_outer_exit
+        move $t4, $zero # counter j is t4
+        print_inner_loop:
+            bge $t4, $a1, print_inner_exit
+            lw $t5, ($t2) # current element is t5
+            addi $t2, $t2, 4
+
+            li $v0, 1
+            move $a0, $t5
+            syscall # prints current integer
+            move $a0, $t0
+
+
+            
+    print_outer_exit:
     jr $ra
 
 transposeMatrix:
