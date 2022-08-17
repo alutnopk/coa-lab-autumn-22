cd4baf963d9715d66cfc1e30847a7a5050544e19
Kartik Pontula <75580066+alutnopk@users.noreply.github.com>
2022-08-17 13:47:09 +0530

    Updated

--- MIPS Assignment 2/Q3_Grp_12.s
+++ MIPS Assignment 2/Q3_Grp_12.s
@@ -8,8 +8,26 @@
 
 # Data Segment
 .data
+prompt_init: .asciiz "Enter four positive integers m, n, a and r: "
 
 # Text Segment
 .text
 .globl main
 main:
+    li $v0, 4
+    la $a0, prompt_init
+    syscall # prompts user for input
+
+    li $v0, 5
+    syscall
+    move $s0, $v0 # m
+    li $v0, 5
+    syscall
+    move $s0, $v0 # n
+    li $v0, 5
+    syscall
+    move $s0, $v0 # a
+    li $v0, 5
+    syscall
+    move $s0, $v0 # r
+    
