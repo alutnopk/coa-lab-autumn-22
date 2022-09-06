50f7ba23e4ce5ee0ea57cda78a34af87d91f7824
Kartik Pontula <75580066+alutnopk@users.noreply.github.com>
2022-09-06 23:40:36 +0530

    basic stack functions, i/o

--- MIPS_Assignment_3/Q1_Grp_12.s
+++ MIPS_Assignment_3/Q1_Grp_12.s
@@ -17,14 +17,21 @@ error_msg: .asciiz "Entered integers should be positive.\n"
 .globl main
 
 # register legend for main:
-# n: $s0
-# a: $s1
-# r: $s2
-# m: $s3
+# n: -8($fp)
+# a: -12($fp)
+# r: -16($fp)
+# m: -20(%fp)
 # matrix address: $s4
 # determinant value: $s5
 
 main:
+	move $s0, $ra
+
+    jal initStack
+
+    move $a0, $s0
+    jal pushToStack # $ra stored in -4($fp)
+
 	li $v0, 4
 	li $a0, prompt1
 	syscall
@@ -32,28 +39,58 @@ main:
 
 	li $v0, 5
     syscall # Accepts n
-    move $s0, $v0
-	blez $s0, error_input #sanity check
+    move $a0, $v0
+	blez $a0, error_input #sanity check
+	jal pushToStack # n stored in -8($fp)
 
 	li $v0, 5
     syscall # Accepts a
-    move $s1, $v0
-	blez $s1, error_input #sanity check
+    move $a0, $v0
+	blez $a0, error_input #sanity check
+	jal pushToStack # a stored in -12($fp)
 
 	li $v0, 5
     syscall # Accepts r
-    move $s2, $v0
-	blez $s2, error_input #sanity check
+    move $a0, $v0
+	blez $a0, error_input #sanity check
+	jal pushToStack # r stored in -16($fp)
 
 	li $v0, 5
     syscall # Accepts m
-    move $s3, $v0
-	blez $s3, error_input #sanity check
+    move $a0, $v0
+	blez $a0, error_input #sanity check
+	jal pushToStack # m stored in -20($fp)
 
-	
+	lw $t0, -8($fp)
+	mult $t0, $t0
+	mflo $a0
+	jal mallocInStack
+	move $s0, $v0 # save address of array in $s0
+
+	move $t0, $a0 # t0 stores n^2
 
 
-	
+initStack:
+	addi $sp, $sp, -4
+	sw $fp, ($sp)
+	move $fp, $sp
+	jr $ra
+
+pushToStack:
+    addi $sp, $sp, -4
+    sw $a0, ($sp)
+    jr $ra
+
+mallocInStack:
+    sll $t0, $a0, 2
+    sub $sp, $sp, $t0
+    move $v0, $sp
+    jr $ra
+
+popFromStack:
+    lw $v0, ($sp)
+	# addi $sp, $sp, 4
+
 
 error_input:
 	li $v0, 4
